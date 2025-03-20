c332e8442347dd92742dd16d66c458b914536b7c
Denis <den.listapad@mail.ru>
2025-03-20 08:53:34 +0300

    add template and static

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from shop.views import first_view
-
+from django.urls import path,re_path
+# from shop.views import first_view,second_view,first_html,index
+from shop.views import home, info
 urlpatterns = [
-    path('hello/', first_view),
+    # path("hello/<path:year>/", first_view),
+    # path("", index),
+    # path("hello/first_html",first_html),
+    # re_path(r"^reg/(?P<year>2[0-9]{3})/$", second_view),
+    path("", home, name="home"),
+    path("info", info, name="info"),
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def first_view(request):
-    return HttpResponse("<h1>Hello Django</h1>")
-# Create your views here.
-# MTV
-# Model => table db
-# Template => html
-# Views => controller
+# users = [
+#     {"user": "Ivan", "age": 24},
+#     {"user": "david", "age": 29},
+#     {"user": "peter", "age": 15},
+#     {"user": "ann", "age": 34},
+#     {"user": "Eva", "age": 31},
+# ]
+#
+#
+#
+# def first_view(request, year):
+#     return HttpResponse(f"<h1>Hello Django {year}</h1>")
+#
+#
+# def second_view(request, year):
+#     return HttpResponse(f"<h1>Hello Django {year}</h1>")
+#
+#
+# def first_html(request):
+#     context = {
+#         "users": users,
+#         "var_str": "hello_context"
+#     }
+#     return render(request, template_name='hello.html', context=context)
+
+def home(request):
+    return render(request, template_name="home.html")
+
+def info(request):
+    return render(request, template_name="info.html")
